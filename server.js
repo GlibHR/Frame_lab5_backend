c9979d01500af60d67c7a97f9c093873d57eb75b
Hlib_HV <165830693+GlibHR@users.noreply.github.com>
2025-12-03 21:17:59 +0200

    Hardcode CORS headers

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./services/db.js";
-import apiRoutes from "./routes/api.js";
+import apiRoutes from "./routes/api.js"; 
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Дозволити всім
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Дозволити всі методи
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"); // Дозволити заголовки
+  
+  if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 
@@ -17,6 +25,5 @@ app.use("/", apiRoutes);
 const dbURI = process.env.MONGO_URI;
 connectDB(dbURI);
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
